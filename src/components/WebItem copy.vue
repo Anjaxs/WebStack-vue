ed08fc768e355e25be3801950de21ef1b519f03b
WuWenbin <541330190@qq.com>
2022-10-23 13:22:41 +0800

    deps: update to vue 3

--- src/components/WebItem copy.vue
+++ src/components/WebItem copy.vue
@@ -6,12 +6,13 @@
     <div class="row">
       <div class="col-sm-3" v-for="(web, idx) in item.web" :key="idx">
         <div
-          class="xe-widget xe-conversations box2 label-info"
           title=""
+          class="webitem"
+          :class="{ hover: web.hover ?? false }"
           @click="openweb(web.url)"
-          data-toggle="tooltip"
-          data-placement="bottom"
           :data-original-title="web.url"
+          @mouseover="handleOverCard(web)"
+          @mouseleave="handleLeaveCard(web)"
         >
           <div class="xe-comment-entry">
             <a class="xe-user-img">
@@ -24,6 +25,7 @@
               <p class="overflowClip_2">{{ web.desc }}</p>
             </div>
           </div>
+          <!-- <div class="webitem-url" v-if="web.hover">{{ web.url }}</div> -->
         </div>
       </div>
     </div>
@@ -39,6 +41,12 @@ export default {
     transName: Function,
   },
   methods: {
+    handleOverCard(web) {
+      web.hover = true;
+    },
+    handleLeaveCard(web) {
+      web.hover = false;
+    },
     openweb(url) {
       window.open(url, "_blank");
     },
@@ -49,5 +57,30 @@ export default {
 <style scoped>
 i {
   margin-right: 7px;
+}
+.webitem {
+  height: 86px;
+  margin-bottom: 0px;
+  padding: 15px;
+  cursor: pointer;
+  background-color: #fff;
+  border-radius: 4px;
+  border: 1px solid #e4ecf3;
+  margin: 20px 0 0 0;
+  transition: all 0.3s ease;
+}
+.webitem.hover {
+  transform: translateY(-6px);
+  box-shadow: 0 26px 40px -24px rgb(0 36 100 / 30%);
+}
+.webitem-url {
+  position: absolute;
+  left: 0;
+  bottom: 0;
+  width: 100%;
+  padding-left: 65px;
+  height: 15px;
+  line-height: 15px;
+  color: #666;
 }
 </style>
